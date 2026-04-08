865a8322dbc5b6b7f62eec1d13d758d41bccd479
OsSatya4 <osikoczki2001@gmail.com>
2026-04-08 10:38:06 +0200

    initial spring boot project structure
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/hu/nye/prog3/game_finder/GameFinderApplication.java
@@ -0,0 +1,13 @@
+package hu.nye.prog3.game_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameFinderApplication.class, args);
+	}
+
+}
