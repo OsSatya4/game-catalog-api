2a58e8ef32165b573cbadb814df870da98738455
OsSatya4 <osikoczki2001@gmail.com>
2026-04-09 10:19:16 +0200

    repository class completed

--- src/main/java/hu/nye/prog3/game_finder/repository/GameRepository.java
+++ src/main/java/hu/nye/prog3/game_finder/repository/GameRepository.java
@@ -1,4 +1,13 @@
 package hu.nye.prog3.game_finder.repository;
 
-public class GameRepository {
+
+import hu.nye.prog3.game_finder.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game,Long> {
+    List<Game> findByTitleContainingIgnoreCase(String title);
 }
