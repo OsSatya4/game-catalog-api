6d5e21a0d44c3ea66b852dc47490ac5a70e8210e
OsSatya4 <osikoczki2001@gmail.com>
2026-04-09 10:19:29 +0200

    contoller class completed

--- src/main/java/hu/nye/prog3/game_finder/controller/GameController.java
+++ src/main/java/hu/nye/prog3/game_finder/controller/GameController.java
@@ -1,4 +1,50 @@
 package hu.nye.prog3.game_finder.controller;
 
+
+import hu.nye.prog3.game_finder.model.Game;
+import hu.nye.prog3.game_finder.service.GameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/games")
+@RequiredArgsConstructor
 public class GameController {
+    private final GameService gameService;
+
+    @GetMapping
+    public List<Game> getAllGames(){
+        return gameService.getAllGames();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Game> getGameById(@PathVariable Long id){
+        return gameService.getGameById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Game createGame(@RequestBody Game game){
+        return gameService.saveGame(game);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Game> updateGame(@PathVariable Long id, @RequestBody Game game){
+        try {
+            return  ResponseEntity.ok(gameService.updateGame(id, game));
+        }catch (RuntimeException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteGame(@PathVariable Long id){
+        gameService.deleteGameById(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
