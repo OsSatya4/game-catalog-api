3d6c393119b391cdab236ecebf084dbb07a70bef
OsSatya4 <osikoczki2001@gmail.com>
2026-04-09 09:53:49 +0200

    category class complited

--- src/main/java/hu/nye/prog3/game_finder/model/Category.java
+++ src/main/java/hu/nye/prog3/game_finder/model/Category.java
@@ -1,4 +1,22 @@
 package hu.nye.prog3.game_finder.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    private List<Game> games;
 }
